55415b6fe080155ea2d25c37e0343acce57471d8
Casey <91210043+peicasey@users.noreply.github.com>
2024-04-21 03:26:41 -0500

    page layout

--- src/app/start/page.tsx
+++ src/app/start/page.tsx
@@ -1,9 +1,9 @@
 import Image from "next/image";
 
-export default function Home() {
+export default function Start() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 gap-16">
-      <h1>BanditTyper</h1>
+
     </main>
   );
 }
