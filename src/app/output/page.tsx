40e5114da45fbbcf5e22a51dd8c8439f2c80ea54
Casey <91210043+peicasey@users.noreply.github.com>
2024-04-28 15:55:10 -0500

    toast added

--- src/app/output/page.tsx
+++ src/app/output/page.tsx
@@ -1,16 +1,25 @@
 "use client";
 
 import Image from "next/image";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useToast } from "@/components/ui/use-toast"
 
 import { poppins } from "@/app/fonts";
 import Keyboard from "@/components/keyboard/keyboard";
 
-export default function Start() {
+export default function Output() {
 
-  const frequentlyUsed = localStorage.getItem('frequentlyUsed') ? localStorage.getItem('frequentlyUsed') : ""
+  const [frequentlyUsed, setFrequentlyUsed] = useState("");
+
+  useEffect(() => {
+    const item = localStorage.getItem('frequentlyUsed') || "";
+    setFrequentlyUsed(item);
+  }, []);
+
 
   const [input, setInput] = useState("");
+  const { toast } = useToast();
+  var currentdate = new Date();
 
   return (
     <main className="flex min-h-screen items-center justify-between p-24 gap-16 bg-lightGreen">
@@ -21,7 +30,20 @@ export default function Start() {
           <p className="text-gray-600">
             Give it a quick try!
           </p>
-        <button className="bg-calmGreen hover:bg-[#7ea77d] text-white text-center font-bold px-4 py-2 rounded-xl duration-200" style={poppins.style}>
+        <button className="bg-calmGreen hover:bg-[#7ea77d] text-white text-center font-bold px-4 py-2 rounded-xl duration-200" 
+          style={poppins.style}
+          onClick={() => {
+            toast({
+              title: "Keyboard Layout",
+              description: "Saved: " + currentdate.getDate() + "/"
+              + (currentdate.getMonth()+1)  + "/" 
+              + currentdate.getFullYear() + " @ "  
+              + currentdate.getHours() + ":"  
+              + currentdate.getMinutes() + ":" 
+              + currentdate.getSeconds(),
+            })
+          }}
+        >
           save keyboard to profile
         </button>
       </div>
